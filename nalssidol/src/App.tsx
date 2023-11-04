4398c7db5bb5766c4447e7c1992bd689450380e3
mk1329 <mk1329@dgu.ac.kr>
2023-11-05 02:02:57 +0900

    :sparkles: Feat: 파.꾸(파비콘 꾸미기)

--- nalssidol/src/App.tsx
+++ nalssidol/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/날알돌.png'
 import './App.css'
 
 function App() {
