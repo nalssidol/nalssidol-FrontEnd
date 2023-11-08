12ef5f486a95596cd2c5b007616c407eea7fe069
Hyeona01 <sha0209@dgu.ac.kr>
2023-11-09 04:55:27 +0900

    ✨ Feat: 지역 선택 navigate

--- src/components/select/NowSelected.tsx
+++ src/components/select/NowSelected.tsx
@@ -1,11 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import * as S from "./styled";
 
 type Props = { city: string; gu: string };
 
 const NowSelected = (props: Props) => {
+  const navigate = useNavigate();
   return (
     <S.NowSelectedWrap>
-      <S.PrevArrow src="/select/prev.png" alt="<" />
+      <S.PrevArrow
+        src="/select/prev.png"
+        alt="<"
+        onClick={() => navigate("/")}
+      />
       <div>
         <S.SubText>현재지역</S.SubText>
         <S.MainText>
