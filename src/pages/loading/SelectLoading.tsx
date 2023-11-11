db72dacfd2037f4b57ce3a7cb17a92599f00d09f
mk1329 <mk1329@dgu.ac.kr>
2023-11-11 15:42:47 +0900

    :lipstick: 로딩화면 완성

--- src/pages/loading/SelectLoading.tsx
+++ src/pages/loading/SelectLoading.tsx
@@ -4,6 +4,9 @@ const SelectLoading = () => {
     return (
         <S.SelectLoadingWrapper>
             <img src="/selectloading.png" alt="선택로딩화면" />
+            <S.LoadingMent>
+                선택한 지역으로 이동 중 ...
+            </S.LoadingMent>
             <div style={{ width: "100%" }}>
                 <S.LoadingDol>
                     <img src="/facedol.png" alt="돌얼굴" />
